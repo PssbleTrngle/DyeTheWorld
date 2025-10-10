e989ec5829e238b7529d2770ce35aed142131f49
Niklas Widmann <nik@somethingcatchy.net>
2025-10-10 19:04:49 +0200

    remove verbose statements

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-import com.possible_triangle.gradle.packwiz.ErrorStrategy
-
 pluginManagement {
     repositories {
         mavenLocal()
@@ -10,12 +8,4 @@ pluginManagement {
 plugins {
     id("com.possible-triangle.helper") version ("1.0.50")
     id("com.possible-triangle.packwiz") version ("1.0.50")
-}
-
-packwiz {
-    verbose = true
-    packs.named("default") {
-        strategy = ErrorStrategy.FAIL
-        from.set { rootDir.resolve("pack") }
-    }
 }
