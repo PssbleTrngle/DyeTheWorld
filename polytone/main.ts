9c7658685b3aa9a338183dac7d40ed4eb02d7372
Niklas Widmann <nik@somethingcatchy.net>
2025-10-19 20:40:38 +0200

    add harnesses to polytone resourcepack

--- polytone/main.ts
+++ polytone/main.ts
@@ -11,7 +11,7 @@ import { resolve } from "path";
 
 const logger = createLogger();
 
-const packFormat = 15;
+const packFormat = 34;
 const loader = new PackLoader(logger, { packFormat });
 await loader.loadRegistryDump(createResolver({ from: resolve("dump") }));
 
@@ -57,8 +57,6 @@ function replace(color: string, base: string) {
   return base.replace("$color", color);
 }
 
-let previous: IdInput<ItemId> | undefined = undefined;
-
 function requireItem(id: IdInput) {
   loader.registries.validateEntry("minecraft:item", id);
   return id as ItemId;
@@ -76,11 +74,8 @@ function addColored(
   loader.tabs.add(tab, ids, {
     file: { namespace: PACK_NAMESPACE, path: mod },
     mods: [mod],
-    after: previous,
   });
   logger.info(`  added ${base}s from ${mod}`);
-
-  previous = ids[ids.length - 1];
 }
 
 addColored("dye_depot", "dye");
@@ -161,6 +156,7 @@ addColored("comforts", "hammock", undefined, append);
 // addColored("dye_the_world", "quark_stool");
 
 addColored("chalk", "chalk");
+addColored("dye_the_world", "harness", "vanillabackport");
 
 addColored("create", "valve_handle");
 addColored("create", "toolbox");
