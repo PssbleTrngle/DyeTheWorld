9585394f51a4d5f0c18f3f9a17500838574fe5c8
Niklas Widmann <nik@somethingcatchy.net>
2026-05-12 07:51:32 +0200

    update vanilla backports and use new bundle api

--- src/main/kotlin/com/possible_triangle/dye_the_world/compat/VanillaBackportsCompat.kt
+++ src/main/kotlin/com/possible_triangle/dye_the_world/compat/VanillaBackportsCompat.kt
@@ -5,18 +5,21 @@ import com.blackgear.vanillabackport.common.api.bundle.BundleFeatures
 import com.possible_triangle.dye_the_world.Constants
 import com.possible_triangle.dye_the_world.Constants.Mods.VANILLA_BACKPORT
 import com.possible_triangle.dye_the_world.extensions.createId
+import com.possible_triangle.dye_the_world.index.DyedVanillaBackport.BUNDLES
 import com.possible_triangle.dye_the_world.index.DyedVanillaBackport.HARNESSES
 import kotlin.collections.component1
 import kotlin.collections.component2
 
 object VanillaBackportsCompat {
-
     fun registerHarnessLayers() {
-        HARNESSES.forEach { (dye, item) ->
+        HARNESSES.forEach { (color, item) ->
             val stack = item.asStack()
-            val texture = Constants.MOD_ID.createId("textures/entity/$VANILLA_BACKPORT/harness/${dye}.png")
+            val texture = Constants.MOD_ID.createId("textures/entity/$VANILLA_BACKPORT/harness/$color.png")
             GhastHarnessHandler.register(stack, texture)
         }
-    }
 
+        BUNDLES.forEach { (color, item) ->
+            BundleFeatures.register(color, item.get())
+        }
+    }
 }
--- src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedVanillaBackport.kt
+++ src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedVanillaBackport.kt
@@ -1,6 +1,5 @@
 package com.possible_triangle.dye_the_world.index
 
-import com.blackgear.vanillabackport.common.level.items.HarnessItem
 import com.blackgear.vanillabackport.common.registries.ModBlocks
 import com.blackgear.vanillabackport.core.data.tags.ModItemTags
 import com.possible_triangle.dye_the_world.*
@@ -14,60 +13,61 @@ import net.minecraft.data.recipes.ShapedRecipeBuilder
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.item.BundleItem
 import net.minecraft.world.item.CreativeModeTabs
+import net.minecraft.world.item.Item
 import net.minecraft.world.item.Items
 import net.minecraft.world.item.component.BundleContents
 import net.minecraft.world.level.block.Blocks
 
 object DyedVanillaBackport {
-
     private val DYES = dyesFor(VANILLA_BACKPORT)
 
     private val TAB = ResourceKey.create(Registries.CREATIVE_MODE_TAB, VANILLA_BACKPORT.createId("vanilla_backport"))
 
-    val HARNESSES = DYES.associateWith { dye ->
-        REGISTRATE.`object`("${dye}_harness")
-            .dyedItem(dye, VANILLA_BACKPORT, ::HarnessItem)
-            .properties { it.stacksTo(1) }
-            .lang("${dye.translation} Harness")
-            .germanLang("${dye.germanTranslation(Genus.I)} Geschirr")
-            .optionalTag(ModItemTags.HARNESSES)
-            .recipe { context, provider ->
-                ShapedRecipeBuilder.shaped(RecipeCategory.MISC, context.get())
-                    .pattern("LLL")
-                    .pattern("G#G")
-                    .define('#', dye.blockOf("wool"))
-                    .define('G', Blocks.GLASS)
-                    .define('L', Items.LEATHER)
-                    .unlockedBy(ModBlocks.DRIED_GHAST.get())
-                    .group("harness")
-                    .save(provider)
-            }
-            .model { c, p ->
-                p.generated(c, Constants.MOD_ID.createId("item/$VANILLA_BACKPORT/harness/$dye"))
-            }
-            .tab(CreativeModeTabs.TOOLS_AND_UTILITIES)
-            .tab(TAB)
-            .register()
-    }
+    val HARNESSES =
+        DYES.associateWith { dye ->
+            REGISTRATE
+                .`object`("${dye}_harness")
+                .dyedItem(dye, VANILLA_BACKPORT, ::Item)
+                .properties { it.stacksTo(1) }
+                .lang("${dye.translation} Harness")
+                .germanLang("${dye.germanTranslation(Genus.I)} Geschirr")
+                .optionalTag(ModItemTags.HARNESSES)
+                .recipe { context, provider ->
+                    ShapedRecipeBuilder
+                        .shaped(RecipeCategory.MISC, context.get())
+                        .pattern("LLL")
+                        .pattern("G#G")
+                        .define('#', dye.blockOf("wool"))
+                        .define('G', Blocks.GLASS)
+                        .define('L', Items.LEATHER)
+                        .unlockedBy(ModBlocks.DRIED_GHAST.get())
+                        .group("harness")
+                        .save(provider)
+                }.model { c, p ->
+                    p.generated(c, Constants.MOD_ID.createId("item/$VANILLA_BACKPORT/harness/$dye"))
+                }.tab(CreativeModeTabs.TOOLS_AND_UTILITIES)
+                .tab(TAB)
+                .register()
+        }
 
-    val BUNDLES = DYES.associateWith { dye ->
-        REGISTRATE.`object`("${dye}_bundle")
-            .dyedItem(dye, VANILLA_BACKPORT, ::BundleItem)
-            .properties { it.stacksTo(1) }
-            .properties { it.component(DataComponents.BUNDLE_CONTENTS, BundleContents.EMPTY) }
-            .lang("${dye.translation} Bundle")
-            .germanLang("${dye.germanTranslation(Genus.I)} Bündel")
-            .optionalTag(ModItemTags.BUNDLES)
-            .model { c, p ->
-                p.generated(c, Constants.MOD_ID.createId("item/$VANILLA_BACKPORT/bundle/$dye"))
-            }
-            .tab(CreativeModeTabs.TOOLS_AND_UTILITIES)
-            .tab(TAB)
-            .register()
-    }
+    val BUNDLES =
+        DYES.associateWith { dye ->
+            REGISTRATE
+                .`object`("${dye}_bundle")
+                .dyedItem(dye, VANILLA_BACKPORT, ::BundleItem)
+                .properties { it.stacksTo(1) }
+                .properties { it.component(DataComponents.BUNDLE_CONTENTS, BundleContents.EMPTY) }
+                .lang("${dye.translation} Bundle")
+                .germanLang("${dye.germanTranslation(Genus.I)} Bündel")
+                .optionalTag(ModItemTags.BUNDLES)
+                .model { c, p ->
+                    p.generated(c, Constants.MOD_ID.createId("item/$VANILLA_BACKPORT/bundle/$dye"))
+                }.tab(CreativeModeTabs.TOOLS_AND_UTILITIES)
+                .tab(TAB)
+                .register()
+        }
 
     fun register() {
         REGISTRATE.register()
     }
-
 }
