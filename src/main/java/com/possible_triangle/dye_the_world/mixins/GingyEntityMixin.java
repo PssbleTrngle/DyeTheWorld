4e6e60f54a928ad4a918395a75a94b93fc33bd4a
Niklas Widmann <nik@somethingcatchy.net>
2026-07-28 11:54:36 +0200

    remap GingyEntityMixin

--- src/main/java/com/possible_triangle/dye_the_world/mixins/GingyEntityMixin.java
+++ src/main/java/com/possible_triangle/dye_the_world/mixins/GingyEntityMixin.java
@@ -8,7 +8,7 @@ import net.minecraft.world.item.DyeColor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = GingyEntity.class, remap = false)
+@Mixin(value = GingyEntity.class)
 public class GingyEntityMixin {
 
     @WrapOperation(
